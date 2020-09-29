45660b2c22b8e9528ed299c458b4491314a08af3
kkevelier <71726070+kkevelier@users.noreply.github.com>
2020-09-29 14:38:08 -0400

    Add files via upload

--- average.py
+++ average.py
@@ -5,7 +5,10 @@ average = 0.0
 scoreCount = 0
 
 numberOfTests = int(input("Please enter the number of tests you want to average: "))
+#make these next 3 lines repeat until scoreCount = numberOfTests
 score = int(input("Please enter a score: "))
 scoreCount = scoreCount + 1
+total = total + score
+
 average = (score1 + score2)/2
 print("The average is ",average)
