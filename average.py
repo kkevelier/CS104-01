a3a6f42c7d3eff261c7420d87d94219300b11446
kkevelier <71726070+kkevelier@users.noreply.github.com>
2020-09-29 14:33:57 -0400

    Second commit of average before adding loop to get all scores

--- average.py
+++ average.py
@@ -1,5 +1,11 @@
+numberOfTests = 0
+score = 0
+total = 0
+average = 0.0
+scoreCount = 0
+
 numberOfTests = int(input("Please enter the number of tests you want to average: "))
-score1 = float(input("Please enter a score: "))
-score2 = float(input("Please enter a score: "))
+score = int(input("Please enter a score: "))
+scoreCount = scoreCount + 1
 average = (score1 + score2)/2
 print("The average is ",average)
