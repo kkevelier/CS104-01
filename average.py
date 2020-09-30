2de9f2eff77a17bea863ec743c8ea93dac5d1b91
kkevelier <71726070+kkevelier@users.noreply.github.com>
2020-09-30 18:37:32 -0400

    Third commit with for loop to calculate average

--- average.py
+++ average.py
@@ -5,10 +5,20 @@ average = 0.0
 scoreCount = 0
 
 numberOfTests = int(input("Please enter the number of tests you want to average: "))
-#make these next 3 lines repeat until scoreCount = numberOfTests
-score = int(input("Please enter a score: "))
-scoreCount = scoreCount + 1
-total = total + score
 
-average = (score1 + score2)/2
+#make these next 3 lines repeat until scoreCount = numberOfTests
+while scoreCount < numberOfTests:
+    score = int(input("Please enter the score: "))
+    
+    #scoreCount is at 0 but 1 is added each time a number is inputed by user
+    scoreCount += 1
+    
+    #total is at 0 but a sum accumulates as the user adds more values
+    total += score
+    
+    #When the number of scores = the initial number inputed the loop stops
+    if scoreCount == numberOfTests:
+        break
+
+average = total/scoreCount
 print("The average is ",average)
